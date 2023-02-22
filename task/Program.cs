cb051dd7c37cd2c3aa45fc5d8267bfb1661993df
Alex Rokachev <alserokachev@gmail.com>
2023-02-22 18:41:06 +0300

    made numb check method

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int numbCheck(string str)
+{
+    int number;
+    string? text;
+    while (true)
+    {
+        System.Console.Write("Введите длину массива: ");
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            return number;
+        }
+        System.Console.WriteLine("Некорректно введены данные");
+    }
+}
+
