5b5d1d3f19fb474cf5b85d69d1ffea5eb9b0601c
Alex Rokachev <alserokachev@gmail.com>
2023-02-22 19:18:37 +0300

    made new methods to solve the task

--- task/Program.cs
+++ task/Program.cs
@@ -20,9 +20,67 @@ string[] fillArrWithElement()
     string[] array = new string[arrLength];
     for (int i = 0; i < arrLength; i++)
     {
-        System.Console.WriteLine("Введите элемент массива: ");
-        array[i] = Console.ReadLine();
+        System.Console.Write("Введите элемент массива: ");
+        string temp = Console.ReadLine();
+        if (temp == string.Empty)
+        {
+            System.Console.WriteLine("Нельзя оставить этот элемент пустым");
+            System.Console.WriteLine();
+            i--;
+        }
+        else
+        {
+            array[i] = temp;
+        }
     }
     return array;
 }
 
+int findSortedArrLength(string[] refArray)
+{
+    int counter = 0;
+    for (int i = 0; i < refArray.Length; i++)
+    {
+        if (refArray[i].Length < 4)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+string[] sortArr(string[] refArray, int sortedArrLength)
+{
+    int counter = 0;
+    string[] array = new string[sortedArrLength];
+    for (int i = 0; i < refArray.Length; i++)
+    {
+        if (refArray[i].Length > 3)
+        {
+            continue;
+        }
+        else
+        {
+            array[counter] = refArray[i];
+            counter++;
+        }
+    }
+    return array;
+}
+
+void printArray(string[] sortedArray)
+{
+    if (sortedArray.Length == 1)
+    {
+        System.Console.WriteLine("Элементы, чья длина меньше 3-х символов отсутствуют");
+    }
+    else
+    {
+        System.Console.WriteLine($"Элементы, чья длина меньше или равна 3-м символам: {String.Join(" , ", sortedArray)}");
+    }
+}
+
+string[] refArray = fillArrWithElement();
+int sortedArrLength = findSortedArrLength(refArray);
+string[] sortedArray = sortArr(refArray, sortedArrLength);
+printArray(sortedArray);
